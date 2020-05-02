c72427da42e654ef8611df28ef39218cca801f62
Aaron Beasley <aaroncb@aaroncb.com>
2020-05-02 01:03:33 -0500

    Initial Commit

---
+++ Maxi-Stats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maxi-Stats
+//
+//  Created by Aaron Beasley on 5/2/20.
+//  Copyright © 2020 Aaron Beasley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
